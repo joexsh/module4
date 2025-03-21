735b0bbcf937725e75529aea1ea4c2ac18bd471b
Sheng Leslie Joevenller <144104430+joexsh@users.noreply.github.com>
2025-03-21 11:53:13 +0100

    Add files via upload
    module 4 hello and goodbye javascript

---
+++ SpeakGoodBye.js
@@ -0,0 +1,36 @@
+// STEP 6: Wrap the entire contents of SpeakGoodBye.js inside of an IIFE
+// STEP 7: Create an object, called 'byeSpeaker' to which you will attach
+// the "speak" method and which you will expose to the global context
+// var byeSpeaker =
+
+// // DO NOT attach the speakWord variable to the 'byeSpeaker' object.
+// var speakWord = "Good Bye";
+
+// // STEP 8: Rewrite the 'speak' function such that it is attached to the
+// // byeSpeaker object instead of being a standalone function.
+
+// function speak(name) {
+//   console.log(speakWord + " " + name);
+// }
+
+// // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
+// // 'byeSpeaker' on the global scope as well.
+
+
+
+// STEP 6: IIFE wrapper
+(function (window) {
+  // STEP 7: Create byeSpeaker object
+  var byeSpeaker = {};
+
+  // Private variable (not attached to byeSpeaker)
+  var speakWord = "Good Bye";
+
+  // STEP 8: Attach speak method to byeSpeaker
+  byeSpeaker.speak = function (name) {
+    console.log(speakWord + " " + name);
+  };
+
+  // STEP 9: Expose to global scope
+  window.byeSpeaker = byeSpeaker;
+})(window);
